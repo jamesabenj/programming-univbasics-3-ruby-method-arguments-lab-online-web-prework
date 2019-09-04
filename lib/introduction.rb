b56c8ef8719b99deb55ca359fa574f4b757b78aa
James Benjamin <james.abenj@gmail.com>
2019-09-04 06:31:54 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -3,7 +3,7 @@
 # thing is NOT to "complete labs," the important thing is to learn the skills you need
 # in order to complete the rest of this course and be ready for the next course!
 
-introduction = name(James)
+#introduction = name(James)
 put "Hi, my name is #{name}."
 
 
